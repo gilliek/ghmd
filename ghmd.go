5cb038632aca9ad41c10738cd10a82986440365a
Kevin Gillieron <gilliek@users.noreply.github.com>
2014-04-28 12:47:18 +0200

    Update ghmd.go
    Fix border collapse in the CSS.

--- ghmd.go
+++ ghmd.go
@@ -61,7 +61,7 @@ dl dd > :last-child { margin-bottom: 0; }
 blockquote { border-left: 4px solid #dddddd; padding: 0 15px; color: #777777; }
 blockquote > :first-child { margin-top: 0; }
 blockquote > :last-child { margin-bottom: 0; }
-table { padding: 0; }
+table { padding: 0; border-collapse: collapse; }
 table tr { border-top: 1px solid #cccccc; background-color: white; margin: 0; padding: 0; }
 table tr:nth-child(2n) { background-color: #f8f8f8; }
 table tr th { font-weight: bold; border: 1px solid #cccccc; text-align: left; margin: 0; padding: 6px 13px; }
